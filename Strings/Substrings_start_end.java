0435ac0e5c1bdd26dcc875a2c049d8828de2c684
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-04-02 10:00:16 +0530

    Create Substrings_start_end.java

---
+++ Strings/Substrings_start_end.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+class Substrings_start_end {
+	 public static void main(String[] args) {
+		 System.out.println("enter");
+		 Scanner sc=new Scanner(System.in);
+		 String A=sc.next();
+		 int k =sc.nextInt();
+		 int j=sc.nextInt();
+		 System.out.println(A.substring(k, j));
+}
+}
