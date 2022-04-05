ebf20f985e3bdc50e5f632f0379210d384222a01
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-04-05 09:45:59 +0530

    Create date-and-time.java

---
+++ loops/date-and-time.java
@@ -0,0 +1,16 @@
+08 05 2015
+import java.util.*;
+
+public class date-and-time {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int month = in.nextInt();
+        int day = in.nextInt();
+        int year = in.nextInt();
+
+        Calendar cal= Calendar.getInstance();
+        cal.set(year,month-1,day);
+        System.out.println(cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()).toUpperCase());
+    }
+}
