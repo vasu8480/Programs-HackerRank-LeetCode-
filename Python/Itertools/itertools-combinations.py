62118bfca06cb720b2aa7d2cadee37e474d9296e
vasu8480 <vasu8480@gmail.com>
2022-11-12 21:28:37 +0530

    iter

--- Python/Itertools/itertools-combinations.py
+++ Python/Itertools/itertools-combinations.py
@@ -1,6 +1,5 @@
 from itertools import combinations
-
 s, n = input().split()
 s = ''.join(sorted(s))
 for i in range(1, int(n)+1):
-	print(*sorted(map(''.join, combinations(s, i))), sep = '\n')	
+	print(*sorted(map(''.join, combinations(s, i))), sep = '\n')
