1198c77ca17d67652390d8250a17d7e83254deb3
vasu8480 <vasu8480@gmail.com>
2022-11-12 11:11:56 +0530

    math

---
+++ Python/Math/pow.py
@@ -0,0 +1,9 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+print(pow(a, b)+pow(c, d))
+#-----------------------------------------------------------#
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+print( a**b + c**d)
