0c755b5e8477447f3178e5cda5ea5b57b079215d
vasu8480 <vasu8480@gmail.com>
2022-11-17 18:19:16 +0530

    diagonal

--- Python/Problem Solving/diagonal-difference.py
+++ Python/Problem Solving/diagonal-difference.py
@@ -1,5 +1,12 @@
 arr=[[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+s=0
+d=0
+for i in range(len(arr)):
+		s+=arr[i][i]
+		d+=arr[i][len(arr)-i-1]
+print( abs(s-d))	
 
+#-------------------------------------------------------------#
 s=0
 d=0
 for i in range(len(arr)):
--- rough.py
+++ rough.py
@@ -3,11 +3,6 @@ arr=[[11, 2, 4], [4, 5, 6], [10, 8, -12]]
 s=0
 d=0
 for i in range(len(arr)):
-		for j in range(0,i+1):
-			if i==j:
-				s+=(arr[i][j])
-		for k in range(len(arr),0,-1):
-			if i==len(arr)-k:
-				d+=(arr[i][k-1])
+		s+=arr[i][i]
+		d+=arr[i][len(arr)-i-1]
 print( abs(s-d))	
-print(d)
