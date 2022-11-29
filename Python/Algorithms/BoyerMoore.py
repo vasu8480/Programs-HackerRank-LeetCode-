7e2d117d4fb20569389a38d629744c0188b345df
vasu8480 <vasu8480@gmail.com>
2022-11-29 13:01:46 +0530

    algo

--- Python/Algorithms/BoyerMoore.py
+++ Python/Algorithms/BoyerMoore.py
@@ -1,17 +1,36 @@
 from typing import List
 import collections
-#BoyerMoore Algorithm
-def majorityElement(self, nums: List[int]) -> int:
-    count = 0
-    res = 0
-    for num in nums:
-        if count == 0:
-            res = num
-        count += 1 if num == res else -1
-    return res
 
+#leetcode(169)
+'''
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+'''
+#boyer-moore algorithm for O(1) space
+
+def majorityElement(self, nums: List[int]) -> int:
+        result = 0
+        count = 0
+        for num in nums:
+            if count == 0:
+                result = num
+            count += 1 if num == result else -1
+        return result
 print(majorityElement(1,[2,2,1,1,1,2,2]))
 
+#--------------------------------------------------------------------
+def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        result = 0
+        maxCount = 0
+
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
+            if count[num] > maxCount:
+                result = num
+            maxCount = max(maxCount,count[num])
+        return result
+print(majorityElement(1,[2,2,1,1,1,2,2]))
 #------------------------------------------------------------
 
 def majorityElement(self, nums: List[int]) -> int:
