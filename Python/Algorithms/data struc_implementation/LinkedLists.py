a19340a2c2ca55aef7ab09f000358aaac49abb4d
vasu8480 <vasu8480@gmail.com>
2022-12-01 09:38:35 +0530

    LL

---
+++ Python/Algorithms/data struc_implementation/LinkedLists.py
@@ -0,0 +1,33 @@
+class Node():
+    
+    def __init__(self,value):
+        
+        self.value = value
+        self.nextNode = None
+firstNode = Node(10)
+secondNode = Node(20)
+thirdNode = Node(30)
+firstNode.nextNode = secondNode
+secondNode.nextNode = thirdNode
+print(firstNode.value)
+print(firstNode.nextNode.value)
+print(secondNode.nextNode.value)
+print(firstNode.nextNode.nextNode.value)
+
+#------------------------------------------------------------
+#doubly linked list
+class DoublyNode():
+    
+    def __init__(self,value):
+        self.value = value
+        self.nextNode = None
+        self.prevNode = None
+firstNode = DoublyNode(40)
+secondNode = DoublyNode(50)
+thirdNode = DoublyNode(60)
+firstNode.nextNode = secondNode
+secondNode.prevNode = firstNode
+secondNode.nextNode = thirdNode
+thirdNode.prevNode = secondNode
+print(firstNode.value)
+print(firstNode.nextNode.value)
