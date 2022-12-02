3caf9593c37d60937df1c9f69bd3be6b402c69d0
vasu8480 <vasu8480@gmail.com>
2022-12-02 08:43:25 +0530

    modified:   rough.py 	modified:   tavant.txt

--- rough.py
+++ rough.py
@@ -1,17 +1,7 @@
-def findDuplicate(self, nums: List[int]) -> int:
-        slowPointer = 0
-        fastPointer = 0
-        while True:
-            slowPointer = nums[slowPointer]
-            fastPointer = nums[nums[fastPointer]]
-            if slowPointer == fastPointer:
-                break
-        
-        secondSlowPointer = 0
-        
-        while True:
-            slowPointer = nums[slowPointer]
-            secondSlowPointer = nums[secondSlowPointer]
-            if slowPointer == secondSlowPointer:
-                return slowPointer
-								
+name='vasu'
+age='24'
+
+print("hi",name,"you")
+
+print(f"hi {name} you r {age}")
+print("hi {} you r {}".format(name,age))
