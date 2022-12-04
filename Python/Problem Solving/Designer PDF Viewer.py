eebc8293d2c4ba425ba9ddc915d5137783db5b0c
vasu8480 <vasu8480@gmail.com>
2022-12-04 14:32:51 +0530

    problrm

---
+++ Python/Problem Solving/Designer PDF Viewer.py
@@ -0,0 +1,7 @@
+def designerPdfViewer(h, word):
+		tallest = 0
+		for letter in word:   #ord() returns the unicode of a character
+				if h[ord(letter) - 97] > tallest: # ord(letter) - 97 is the index of the letter in the list
+						tallest = h[ord(letter) - 97] # if the height of the letter is greater than the current tallest, set it as the tallest
+		return tallest * len(word) # return the area of the word
+print(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 1, 1, 5, 5, 1, 5, 2, 5, 5, 5, 5, 5, 5], 'zbc'))
--- rough.py
+++
@@ -1,9 +0,0 @@
-def utopianTree(n):
-		height = 1
-		for i in range(n):
-				if i % 2 == 0:
-						height *= 2
-				else:
-						height += 1
-		return height
-print(utopianTree(0))
