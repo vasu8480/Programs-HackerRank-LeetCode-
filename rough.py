b178cddbe3ae4f5acea7005a0ac9349eacecf7ba
vasu8480 <vasu8480@gmail.com>
2022-12-04 17:59:24 +0530

    problem

--- rough.py
+++ rough.py
@@ -1,3 +1,15 @@
-def permutationEquation(p):
-    
-print(permutationEquation([2,3,1]))
+def findDigits(n):
+    count = 0 
+    num = n 
+    while(num>0): 
+      dig = num%10 
+      if dig > 0 and n%dig == 0: 
+        count=count+1 
+        num = num//10
+    return count
+    for i in str(k):
+        if int(i) > 0 and k % int(i) == 0:
+            count +=1
+    return count
+
+print(findDigits(1012))
