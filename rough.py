fa3285ce5a91e1228844f43b2bb9ba585b82f6fa
vasu8480 <vasu8480@gmail.com>
2022-12-05 21:32:30 +0530

    tav

--- rough.py
+++ rough.py
@@ -1,3 +1,11 @@
-def flippingBits(n):
-    return n ^ 0xffffffff # 32-bit unsigned integer
-print(flippingBits(802743475))
+#Write Algorithm and Flow Chart to accept a student’s name and scores in three subjects.
+
+#Algorithm
+Step 1: Start
+Step 2: Declare variables name, score1, score2, score3, total, average
+Step 3: Input name, score1, score2, score3
+Step 4: Calculate total = score1 + score2 + score3
+Step 5: Calculate average = total / 3
+Step 6: Display name, score1, score2, score3, total, average
+Step 7: Stop
+
