813a5ecf7b4d92f18ca8de4b2f9df5c267054c99
vasu8480 <vasu8480@gmail.com>
2022-12-08 09:34:25 +0530

    226

--- rough.py
+++ rough.py
@@ -1,7 +1,2 @@
-class Solution:
-    def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        while left < right:
-            right = right & (right - 1)
-        return right
-
-print(Solution().rangeBitwiseAnd(5,7))
+l=[1,2,3,1,2,3]
+print(l.count())
