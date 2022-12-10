24e50f583c2fbbbea8bf2bd7967c44eb49228de9
vasu8480 <vasu8480@gmail.com>
2022-12-10 18:50:13 +0530

    8

--- leet_own/String to Integer (atoi)(8).py
+++ leet_own/String to Integer (atoi)(8).py
@@ -21,4 +21,13 @@ class Solution:
         if res < -2**31:
             return -2**31
         return res
-        
+s=Solution()
+print(s.myAtoi("42"))
+print(s.myAtoi("   -42"))
+print(s.myAtoi("4193 with words"))
+print(s.myAtoi("words and 987"))
+print(s.myAtoi("-91283472332"))
+print(s.myAtoi("3.14159"))
+print(s.myAtoi("+-2"))
+print(s.myAtoi("  -0012a42"))
+print(s.myAtoi("  +0 123"))
