03087ed0427b84263de82b4ddfc081dca3dd54bc
vasu8480 <vasu8480@gmail.com>
2022-12-10 11:17:47 +0530

    Graph

--- rough.py
+++ rough.py
@@ -1,38 +1,54 @@
-class Solution:
-    answer = -float("inf")
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.dfs(root)
-        return self.answer
-    
-    def dfs(self,node): #time: O(n), space: O(n)
-        if node is None:
-            return 0
-        
-        left = self.dfs(node.left) 
-        right = self.dfs(node.right)
-        
-        left = max(left,0)
-        right = max(right,0)
-        
-        self.answer = max(self.answer,node.val+left+right)
-        
-        return node.val + max(left,right)
-#-------------------------------------------------------------
-class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.max_sum = float('-inf') 
-        self.dfs(root)
-        return self.max_sum
+class Graph:
+    def __init__(self):
+        self.adjDict = {}
 
-    def dfs(self, node): #time: O(n), space: O(n)
-        if not node:
-            return 0
-        left = max(self.dfs(node.left), 0)
-        right = max(self.dfs(node.right), 0)
-        self.max_sum = max(self.max_sum, node.val + left + right)
-        return node.val + max(left, right)
+    def addVertex(self, vertex):
+        if vertex not in self.adjDict.keys():
+            self.adjDict[vertex] = []
+            return True
+        return False
 
-    
-    
-    
+    def addEdge(self, v1, v2):
+        if v1 in self.adjDict.keys() and v2 in self.adjDict.keys():
+            self.adjDict[v1].append(v2)
+            self.adjDict[v2].append(v1)
+            return True
+        return False
 
+    def removeEdge(self, v1, v2):
+        if v1 in self.adjDict.keys() and v2 in self.adjDict.keys():
+            try:
+                self.adjDict[v1].remove(v2)
+                self.adjDict[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def removeVertex(self, vertex):
+        if vertex in self.adjDict.keys():
+            for relatedVertex in self.adjDict[vertex]:
+                self.adjDict[relatedVertex].remove(vertex)
+            del self.adjDict[vertex]
+            return True
+        return False
+    
+    def printGraph(self):
+        for vertex in self.adjDict:
+            print(vertex, '->', self.adjDict[vertex]) 
+myGraph = Graph()
+myGraph.addVertex('A')
+myGraph.addVertex('B')
+myGraph.addVertex('C')
+myGraph.addVertex('D')
+
+myGraph.addEdge('A', 'B')   
+myGraph.addEdge('A', 'C')
+myGraph.addEdge('B', 'D')
+myGraph.addEdge('C', 'D')
+print(myGraph.adjDict)
+myGraph.printGraph()
+
+myGraph.removeEdge('A', 'B')
+print(myGraph.adjDict)
+myGraph.printGraph()
