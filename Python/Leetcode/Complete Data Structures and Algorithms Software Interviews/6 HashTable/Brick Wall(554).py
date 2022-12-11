12fbc3fb1beb890515c0771f2a687d9841b3ae3d
vasu8480 <vasu8480@gmail.com>
2022-12-11 19:13:34 +0530

    554

--- Python/Leetcode/Complete Data Structures and Algorithms Software Interviews/6 HashTable/Brick Wall(554).py
+++ Python/Leetcode/Complete Data Structures and Algorithms Software Interviews/6 HashTable/Brick Wall(554).py
@@ -1,14 +1,13 @@
 class Solution:
     def leastBricks(self, wall):
-        myHashMap = {0:0} # key: ix , value -> number of gaps
-        
+        m = {} # key: ix , value -> number of gaps
         for row in wall:
             gapCount = 0
             for brick in row[:-1]: #not including the far right index
                 gapCount += brick
-                myHashMap[gapCount] = 1 + myHashMap.get(gapCount,0)
-        return len(wall) - max(myHashMap.values()) if myHashMap else len(wall) #runtime is less than O(n^2) when compared to below
-        #return len(wall) - max(myHashMap.values()) #runtime is more when compared to above
+                m[gapCount] = 1 + m.get(gapCount,0)
+        return len(wall) - max(m.values()) if m else len(wall) #runtime is less than O(n^2) when compared to below
+        #return len(wall) - max(m.values()) #runtime is more when compared to above
 print(Solution().leastBricks([[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]))
 #-----------------------------------------------------------------------------------------
 from collections import Counter
--- rough.py
+++ rough.py
@@ -1,18 +1,27 @@
+from collections import Counter
+
+
 class Solution:
-    def twoSum(self, nums, target):
-        m = {}
-        n = len(nums)
-        for i in range(0,n):
-            goal = target - nums[i]
-            if(goal in m):
-                return [m[goal], i]
-            m[nums[i]] = i
-print(Solution().twoSum([2, 7, 11, 15], 9))
-#---------------------------------------------------
+    def leastBricks(self, wall):
+        cnt = Counter()
+        for row in wall:
+            pos = 0
+            for brick in row[:-1]:
+                pos += brick
+                cnt[pos] += 1
+        return len(wall) - cnt.most_common(1)[0][1] if cnt else len(wall)
+print(Solution().leastBricks([[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]))
+#-----------------------------------------------------------------------------------------
+
 class Solution:
-    def twoSum(self, nums,target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-print(Solution().twoSum([2, 7, 11, 15], 9))
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        myHashMap = {0:0} # key: ix , value -> number of gaps
+        
+        for row in wall:
+            gapCount = 0
+            for brick in row[:-1]: #not including the far right index
+                gapCount += brick
+                myHashMap[gapCount] = 1 + myHashMap.get(gapCount,0)
+        return len(wall) - max(myHashMap.values()) if myHashMap else len(wall) #runtime is less than O(n^2) when compared to below
+        #return len(wall) - max(myHashMap.values()) #runtime is more when compared to above
+print(Solution().leastBricks([[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]))
