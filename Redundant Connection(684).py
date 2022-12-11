90f15f809ae4fd9286cb55b0d7b1fa6dcc28fedb
vasu8480 <vasu8480@gmail.com>
2022-12-11 13:00:30 +0530

    684

--- Redundant Connection(684).py
+++ Redundant Connection(684).py
@@ -35,6 +35,7 @@ class Solution:
 s=Solution()
 print(s.findRedundantConnection([[1,2], [1,3], [2,3]]))
 print(s.findRedundantConnection([[1,2], [2,3], [3,4], [1,4], [1,5]]))
+#---------------------------------------------------------------------------------------------------
 
 class Solution:
     def findRedundantConnection(self, edges): # Union Find
