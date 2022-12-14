48447206d9cb407a69aab8ca23da59ca130f68ce
vasu8480 <vasu8480@gmail.com>
2022-12-14 09:57:27 +0530

    1047

--- leet_own/Remove All Adjacent Duplicates In String(1047).py
+++ leet_own/Remove All Adjacent Duplicates In String(1047).py
@@ -2,22 +2,22 @@ class Solution:
     def removeDuplicates(self, s: str) -> str:
         k=""
         for i in range(len(s)):
-            if k=="":
-                k=k+s[i]
-            elif k[-1]==s[i]:
-                k=k[:-1]
-            else:
-                k=k+s[i]
+            if k=="":   # if k is empty
+                k=k+s[i]    # add the first element
+            elif k[-1]==s[i]:  # if the last element of k is equal to the current element
+                k=k[:-1]    # remove the last element of k
+            else:   # if the last element of k is not equal to the current element
+                k=k+s[i]    # add the current element to k
         return k
 print(Solution().removeDuplicates("abbaca"))
 #-------------------------------------------------
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        stack = []
-        for c in s:
-            if stack and stack[-1] == c:
-                stack.pop()
-            else:
-                stack.append(c)
+        stack = []  # create an empty stack
+        for c in s: # for each character in s
+            if stack and stack[-1] == c:    # if the stack is not empty and the last element of the stack is equal to the current character
+                stack.pop() # remove the last element of the stack
+            else:   # if the stack is empty or the last element of the stack is not equal to the current character
+                stack.append(c) # add the current character to the stack
         return ''.join(stack)
 print(Solution().removeDuplicates("abbaca"))
