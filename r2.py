f92c6ca744f13f7787d3831ce618e654f15de9ac
vasu8480 <vasu8480@gmail.com>
2022-12-17 17:01:50 +0530

    see

--- r2.py
+++ r2.py
@@ -1,30 +1,6 @@
-class Solution:
-    def searchInsert(self, nums,target):
-        for i in range(len(nums)):
-            if target==nums[i] or target<nums[i]:
-                return i
-        return len(nums)
-print(Solution().searchInsert([1,3,5,6], 5))
-#---------------------------------------------------------
-class Solution:
-    def searchInsert(self, nums,target):
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
-#---------------------------------------------------------
-print(Solution().searchInsert([1,3,5,6], 5))
-class Solution:
-    def searchInsert(self, nums,target):
-        for i in range(len(nums)):
-            if nums[i] >= target:
-                return i
-        return len(nums)
-print(Solution().searchInsert([1,3,5,6], 5))
-#---------------------------------------------------------
-class Solution:
-    def searchInsert(self, nums,target):
-        return len([x for x in nums if x < target])
-print(Solution().searchInsert([1,3,5,6], 5))
+n=13
+a=[x for x in range(1,n+1)]
+print(a)
+
+#count number of elements that has 1 in it
+print(sum([1 for x in a if '1' in str(x)]))
