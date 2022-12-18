7b3f6d6ab467d3d38786185cbf415896bddf8a0f
vasu8480 <vasu8480@gmail.com>
2022-12-18 14:55:46 +0530

    347

--- r2.py
+++ r2.py
@@ -1,29 +1,26 @@
-import collections
 class Solution:
-    def topKFrequent(self, nums, k):
-        # 1. count the frequency of each number
-        # 2. sort the numbers by frequency
-        # 3. return the top k numbers
-        count = collections.Counter(nums)
-        return sorted(count.keys(), key=lambda x: count[x], reverse=True)[:k]
-print(Solution().topKFrequent([1,1,1,2,2,3], 2))
+    def productExceptSelf(self, nums):
+        # 1. calculate the product of all numbers
+        # 2. divide the product by each number
+        # 3. return the list of products
+        product = 1
+        for num in nums:
+            product *= num
+        return [product/num for num in nums]
+print(Solution().productExceptSelf([1,2,3,4]))
 #--------------------------------------------------------------------------------
-import collections
 class Solution:
-    def topKFrequent(self, nums,k):
-        # 1. count the frequency of each number
-        # 2. sort the numbers by frequency
-        # 3. return the top k numbers
-        count = collections.Counter(nums)
-        return [i[0] for i in count.most_common(k)] # most_common() returns a list of tuples
-print(Solution().topKFrequent([1,1,1,2,2,3], 2))
-#--------------------------------------------------------------------------------
-import heapq
-class Solution:
-    def topKFrequent(self, nums,k):
-        # 1. count the frequency of each number
-        # 2. sort the numbers by frequency
-        # 3. return the top k numbers
-        count = collections.Counter(nums)
-        return heapq.nlargest(k, count.keys(), key=count.get) # nlargest returns a list of keys
-print(Solution().topKFrequent([1,1,1,2,2,3], 2))
+    def productExceptSelf(self, nums):
+        # 1. calculate the product of all numbers
+        # 2. divide the product by each number
+        # 3. return the list of products
+        product = 1
+        for i in range(len(nums)):
+            product *= nums[i]
+            nums[i] = product
+        product = 1
+        for i in range(len(nums)-1, -1, -1):
+            nums[i] = nums[i-1] * product if i > 0 else product
+            product *= nums[i]
+        return nums
+print(Solution().productExceptSelf([1,2,3,4]))
